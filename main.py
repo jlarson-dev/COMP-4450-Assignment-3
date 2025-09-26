00356f8e23fbae47566c69b158cecfe91b2222e5
Jordan Larson <imjordan11@gmail.com>
2025-09-26 15:34:43 -0400

    added health, predict, and predict_proba endpoints

--- main.py
+++ main.py
@@ -1,2 +1,72 @@
-import fastapi
+from fastapi import FastAPI, HTTPException, Request
+from pydantic import BaseModel
+import joblib
+import time
+import numpy as np
 
+# API
+app = FastAPI(
+    title = "Movie Review Sentiment Analyzer"
+)
+
+# Load the model
+try:
+    model = joblib.load('sentiment_model.pkl')
+    print("Model 'sentiment_model' loaded successfully.")
+except:
+    print("Error: Model file 'sentiment_model.pkl' not found.")
+    model = None
+class Review(BaseModel):
+    text: str
+
+# Helper functions
+def model_state():
+    if model is None:
+        raise HTTPException(status_code=500, detail="Model not loaded.")
+    return model
+
+def predict_sentiment(text: str):
+    """Predict sentiment of input review"""
+    model_state()
+    return model.predict([text])[0]
+
+def pred_proba(text: str):
+    """Returns confidence score of prediction"""
+    model_state()
+    return model.predict_proba([text])[0]
+
+# Middleware runs with each request to add headers
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+@app.get("/health")
+def health():
+    return {"status": "ok", "message": "API is running"}
+
+
+@app.post("/predict")
+def post_pred(input: Review):
+    """Performs a sentiment prediction on an input string"""
+    predicted = predict_sentiment(input.text)
+    return {"sentiment": predicted[0]}
+
+
+@app.post("/predict_proba")
+def post_proba(input: Review):
+    """Takes a text input and returns the predicted sentiment along with its confidence score."""
+    if model is None:
+        raise HTTPException(status_code=500, detail="Model not loaded.")
+    sentiment = predict_sentiment(input.text)
+    probs = pred_proba(input.text)
+    return {"sentiment": sentiment, "probability": round(probs.max(), 2)}
+
+
+# @app.get("/example")
+def get_example():
+    """Returns a random review from the original IMDB training dataset."""
+    pass
